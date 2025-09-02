dc0a3fbbcab4ddf70ae85e12a21fa14efe0914a4
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2025-09-02 15:56:20 +1000

    Fix bad data passing into worker

--- opfs.worker.js
+++ opfs.worker.js
@@ -1,11 +1,24 @@
+async function getRootDirectory() {
+  try {
+    const root = await navigator.storage.getDirectory();
+    return { root, err: null };
+  } catch (error) {
+    return { root: null, err: "Error getting OPFS root: " + error.message };
+  }
+}
+
 self.onmessage = async (e) => {
-  const { fileHandle, data } = e.data;
+  const { root, err } = await getRootDirectory();
+  if (err) throw new Error("Failed to get root directory: " + err);
+  const { path, data } = e.data;
+  const fileHandle = await root.getFileHandle(path, { create: true });
   const handle = await fileHandle.createSyncAccessHandle();
   const enc = new TextEncoder();
-  const bytes = typeof data === 'string' ? enc.encode(data) : new Uint8Array(data);
+  const bytes =
+    typeof data === "string" ? enc.encode(data) : new Uint8Array(data);
   await handle.truncate(0);
   await handle.write(bytes, { at: 0 });
   await handle.flush?.();
   await handle.close();
-  self.postMessage('ok');
+  self.postMessage("ok");
 };
